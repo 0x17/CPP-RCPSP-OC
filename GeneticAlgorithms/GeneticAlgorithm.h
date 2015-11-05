19e897d34d6040762f3e1bd7f7ac14339cc5402f
André Schnabel <andreschnabel@me.com>
2015-11-05 18:37:36 +0100

    Added time limit option for genetic algorithms

--- GeneticAlgorithms/GeneticAlgorithm.h
+++ GeneticAlgorithms/GeneticAlgorithm.h
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iostream>
 #include "../ProjectWithOvertime.h"
+#include "../Stopwatch.h"
 
 using namespace std;
 
@@ -22,8 +23,9 @@ public:
 protected:
     ProjectWithOvertime &p;
     const int numGens, popSize, pmutate;
+	const double timelimit;
 
-    GeneticAlgorithm(ProjectWithOvertime &_p) : p(_p), numGens(80), pmutate(5), popSize(100) {}
+    GeneticAlgorithm(ProjectWithOvertime &_p) : p(_p), numGens(80), pmutate(5), popSize(100), timelimit(-1.0) {}
 
 	void generateChildren(vector<pair<Individual, float>> & population);
 
@@ -65,20 +67,16 @@ void GeneticAlgorithm<Individual>::generateChildren(vector<pair<Individual, floa
 
 template<class Individual>
 pair<vector<int>, float> GeneticAlgorithm<Individual>::solve() {
+	Stopwatch sw;
+	sw.start();
     vector<pair<Individual, float>> pop(popSize*2);
 
-    for(int i=0; i<popSize; i++) {
+    for(int i=0; i<popSize*2; i++) {
         pop[i].first = init(i);
-        pop[i].second = -fitness(pop[i].first);
+		pop[i].second = i < popSize ? -fitness(pop[i].first) : 0.0f;
     }
 
-	for(int i=popSize; i<popSize*2; i++) {
-		pop[i].first = init(i);
-		pop[i].second = 0.0f;
-	}
-
-    for(int i=0; i<numGens; i++) {
-		cout << "Generation " << (i + 1) << endl;
+    for(int i=0; (numGens == -1 || i<numGens) && (timelimit == -1.0 || sw.look() < timelimit); i++) {		
         generateChildren(pop);
 
         for(int j=popSize; j<popSize*2; j++) {
@@ -88,7 +86,8 @@ pair<vector<int>, float> GeneticAlgorithm<Individual>::solve() {
         }
 
 		sort(pop.begin(), pop.end(), [](auto &left, auto &right) { return left.second < right.second; });
-		cout << "Obj=" << -pop[0].second << endl;
+
+		cout << "\rGeneration " << (i + 1) << " Obj=" << -pop[0].second;
     }
 
     auto best = pop[0];
@@ -109,8 +108,7 @@ void GeneticAlgorithm<Individual>::onePointCrossover(vector<int> &motherOrder, v
     for(int i=0; i<=q; i++)
 		daughterOrder[i] = motherOrder[i];
 
-    int ctr = q+1;
-    for(int i=0; i<fatherOrder.size(); i++) {
+    for(int i=0, ctr=q+1; i<fatherOrder.size(); i++) {
         if(!Utils::rangeContains(motherOrder, 0, q, fatherOrder[i])) {
             daughterOrder[ctr] = fatherOrder[i];
             ctr++;
@@ -118,7 +116,6 @@ void GeneticAlgorithm<Individual>::onePointCrossover(vector<int> &motherOrder, v
     }
 }
 
-// FIXME: Contains bug making feasible orders infeasible!
 template<class Individual>
 void GeneticAlgorithm<Individual>::neighborhoodSwap(vector<int> &order) {
     for(int i=1; i<p.numJobs; i++)
--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 #include "LSSolver.h"
 #include "ScheduleVisualizer.h"
 #include "GeneticAlgorithms/OvertimeBound.h"
+#include "Stopwatch.h"
 
 int main(int argc, const char * argv[]) {
-    ProjectWithOvertime p("QBWLBeispiel.DAT");
+    ProjectWithOvertime p("../../Projekte/j120/j1201_1.sm");
 	FixedCapacityGA ga(p);
+	
+	Stopwatch sw;
+	sw.start();
 	auto pair = ga.solve();
+	double solvetime = sw.look();
+	cout << endl << "Solvetime = " << solvetime << " ms" << endl;
+
 	/*if (argc == 2) {
 		ProjectWithOvertime p(argv[1]);
 		LSSolver::writeLSPModelParamFile(p, "LSInstance.txt");
@@ -16,10 +23,10 @@ int main(int argc, const char * argv[]) {
     //auto optimalSts = LSSolver::solve(p);
 	//auto optimalSts = LSSolver::solve2(p);
 	//auto optimalSts = LSSolver::solve3(p);
-	Utils::serializeSchedule(pair.first, "myschedulebiatch.txt");
+	//Utils::serializeSchedule(pair.first, "myschedulebiatch.txt");
     //vector<int> optimalSts(p.numJobs);
     //ScheduleVisualizer::drawScheduleToPDF(p, optimalSts, "out.pdf");
 	//cin.get();
-	system("C:\\Users\\a.schnabel\\Dropbox\\Arbeit\\Scheduling\\Code\\ScheduleVisualizer\\ScheduleVisualizerCommand.exe QBWLBeispiel.DAT myschedulebiatch.txt");
+	//system("C:\\Users\\a.schnabel\\Dropbox\\Arbeit\\Scheduling\\Code\\ScheduleVisualizer\\ScheduleVisualizerCommand.exe C:\\Users\\a.schnabel\\Dropbox\\Arbeit\\Scheduling\\Projekte\\j30\\j301_1.sm myschedulebiatch.txt");
     return 0;
 }
